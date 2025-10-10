c61259c4287b7fa85828da10efe3de55f2f82bd4
CocoTapi <shioriavino@gmail.com>
2025-10-10 13:06:04 +0900

    add links

--- section-2/app/page.js
+++ section-2/app/page.js
@@ -1,9 +1,21 @@
+import Link from "next/link";
+import Header from "./header";
+
 export default function Home() {
+  console.log("Executing...")
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
+      <Header />
+
+      <div>
+        {/* regular a cause reload */}
+        <a href="/about">
+        About Us
+        </a>
+
+        {/* no reload */}
+        <Link href="/about">About Us</Link>
+      </div>
     </main>
   );
 }
